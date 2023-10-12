14b715a0eb993be4de5a641c254b33000c0c8e8e
TanviiSaraf <114498275+TanviiSaraf@users.noreply.github.com>
2023-10-12 14:10:22 +0530

    Update functionconstructer.js

--- functionconstructer.js
+++ functionconstructer.js
@@ -1,12 +1,1 @@
-function Person(firstName, lastName, age) {
-this.firstName = firstName;
-this.lastName = lastName;
-this.age = age;
-this.getFullName = function() {
-return this.firstName + " " + this.lastName;
-};
-}
-const person1 = new Person("Tanvi", "Saraf", 20);
-console.log("Method 1:");
-console.log("Full Name:", person1.getFullName());
-console.log("Age:", person1.age);
+
